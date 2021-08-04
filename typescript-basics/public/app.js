50a074e423053638607e4514dcb409887be8edc0
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-08-04 19:02:08 +0545

    typescript crash course - classes, interface and rendering html template

---
+++ typescript-basics/public/app.js
@@ -0,0 +1,26 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplates.js";
+import { Payment } from "./classes/Payment.js";
+// DOM & type casting
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let doc;
+    //tuples
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
+    if (type.value === "invoice") {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, "end");
+});
